0580a712f4eee2e8f1b88ffd89af390e7f38cd69
CreativeKittens <akbarsani.dev@gmail.com>
2023-11-23 17:01:56 +0700

    change terminal

--- lua/plugin/plugins/terminal.lua
+++ lua/plugin/plugins/terminal.lua
@@ -22,7 +22,7 @@ return {
 	opts = function()
 		local mocha = require("catppuccin.palettes").get_palette("mocha")
 		return {--[[ things you want to change go here]]
-			size = 60,
+			size = 20,
 			shell = vim.o.shell,
 			open_mapping = [[<A-'>]],
 			autochdir = true, -- when neovim changes it current directory the terminal will change it's own when next it's opened0
@@ -42,7 +42,7 @@ return {
 				enabled = true,
 			},
 			hide_number = false,
-			direction = "vertical",
+			direction = "horizontal",
 			float_opts = {
 				border = "curved",
 				winblend = 0,
